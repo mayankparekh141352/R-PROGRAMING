57c64854b95864f003727d7801b3201bee9ce421
mayankparekh141352 <mayank.parekh141352@marwadiuniversity.ac.in>
2026-04-06 13:08:51 +0530

    Add histogram example in R

---
+++ histogram_example.R
@@ -0,0 +1,12 @@
+# Histogram Example in R
+
+# Generate sample data
+marks <- c(45, 60, 70, 55, 80, 90, 75, 65, 85, 50)
+
+# Create histogram
+hist(marks,
+     main = "Histogram of Student Marks",
+     xlab = "Marks",
+     ylab = "Frequency")
+
+# End of program
